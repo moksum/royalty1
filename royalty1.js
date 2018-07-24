3e19451ca03f69c632b7ed7274c4a5ba122b48ba
moksum <gdaiuto@gmail.com>
2018-07-24 04:16:09 -0400

    new royalty javascript and jquery

--- royalty1.js
+++ royalty1.js
@@ -3,7 +3,21 @@
 const olive = document.getElementById('olive');
 
 olive.addEventListener('click', () => {
-  olive.style.color = 'red';
+  olive.style.color = 'yellow';
 
 });
 
+$(document).ready(function() {
+    $(".regnum2").hover(function() {
+        $(this).css("background-color", "pink");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+});
+$(document).ready(function() {
+    $(".little").hover(function() {
+        $(this).css("background-color", "yellow");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+});
